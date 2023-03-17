82d90cea876c40979b31bd6ab039bfbdf3c7a7da
Madhumitha <madhuabi01@yahoo.com>
2023-03-17 17:27:00 +0530

    changes

--- model_codes/cancer.py
+++ model_codes/cancer.py
@@ -1,76 +1,37 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn import ensemble
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 
-from sklearn import preprocessing 
-from sklearn.preprocessing import LabelEncoder
+df = pd.read_csv(r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\data\cancer.csv")
+df.drop(df.columns[[0,-1]], axis=1, inplace=True)
+# Split the features data and the target 
+Xdata = df.drop(['diagnosis'], axis=1)
+ydata = df['diagnosis']
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import datasets, linear_model, metrics
-from sklearn.metrics import  confusion_matrix
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
+# Encoding the target value 
+yenc = np.asarray([1 if c == 'M' else 0 for c in ydata])
+cols = ['concave points_mean','area_mean','radius_mean','perimeter_mean','concavity_mean',]
+Xdata = df[cols]
+print(Xdata.columns)
 
-df = pd.read_csv(r"C:\Users\Madhumitha\Desktop\MajorProject\dataset\Lung_data\lung_cancer.csv")
-df.rename(columns = {'GENDER' : 'GENDER', 'AGE' : 'AGE', 'SMOKING' : 'SMOKING', 'YELLOW_FINGERS' : 'YELLOW_FINGERS', 'ANXIETY' : 'ANXIETY',
-       'PEER_PRESSURE' : 'PEER_PRESSURE', 'CHRONIC DISEASE' : 'CHRONIC DISEASE', 'FATIGUE ' : 'FATIGUE', 'ALLERGY ' : 'ALLERGY', 'WHEEZING' : 'WHEEZING',
-       'ALCOHOL CONSUMING' : 'ALCOHOL CONSUMING', 'COUGHING' : 'COUGHING', 'SHORTNESS OF BREATH' : 'SHORTNESS OF BREATH',
-       'SWALLOWING DIFFICULTY' : 'SWALLOWING DIFFICULTY', 'CHEST PAIN' : 'CHEST PAIN', 'LUNG_CANCER': 'LUNG_CANCER'}, inplace = True)
+X_train, X_test, y_train, y_test = train_test_split(Xdata, yenc, 
+                                                    test_size=0.3,
+                                                    random_state=43)
+print('Shape training set: X:{}, y:{}'.format(X_train.shape, y_train.shape))
+print('Shape test set: X:{}, y:{}'.format(X_test.shape, y_test.shape))
 
-df=pd.get_dummies(data=df,columns=['LUNG_CANCER'],drop_first=True)
-df1 = df.groupby('AGE').agg({ 'GENDER' : 'count', 'SMOKING' : 'sum', 'YELLOW_FINGERS' : 'sum', 'ANXIETY' : 'sum',
-       'PEER_PRESSURE' : 'sum', 'CHRONIC DISEASE' : 'sum', 'FATIGUE' : 'sum', 'ALLERGY' : 'sum', 'WHEEZING' : 'sum',
-       'ALCOHOL CONSUMING' : 'sum', 'COUGHING' : 'sum', 'SHORTNESS OF BREATH' : 'sum',
-       'SWALLOWING DIFFICULTY' : 'sum', 'CHEST PAIN' : 'sum', 'LUNG_CANCER_YES' : 'sum'})
-df2 = df.groupby('GENDER').agg({ 'AGE' : 'count', 'SMOKING' : 'sum', 'YELLOW_FINGERS' : 'sum', 'ANXIETY' : 'sum',
-       'PEER_PRESSURE' : 'sum', 'CHRONIC DISEASE' : 'sum', 'FATIGUE' : 'sum', 'ALLERGY' : 'sum', 'WHEEZING' : 'sum',
-       'ALCOHOL CONSUMING' : 'sum', 'COUGHING' : 'sum', 'SHORTNESS OF BREATH' : 'sum',
-       'SWALLOWING DIFFICULTY' : 'sum', 'CHEST PAIN' : 'sum', 'LUNG_CANCER_YES' : 'sum'})
-df=pd.get_dummies(data=df,columns=['GENDER'],drop_first=True)
-df.drop_duplicates(inplace=True)
-df3 = df.groupby('SMOKING').agg({ 'GENDER_M' : 'sum','AGE' : 'count', 'SMOKING' : 'sum', 'YELLOW_FINGERS' : 'sum', 'ANXIETY' : 'sum',
-       'PEER_PRESSURE' : 'sum', 'CHRONIC DISEASE' : 'sum', 'FATIGUE' : 'sum', 'ALLERGY' : 'sum', 'WHEEZING' : 'sum',
-       'ALCOHOL CONSUMING' : 'sum', 'COUGHING' : 'sum', 'SHORTNESS OF BREATH' : 'sum',
-       'SWALLOWING DIFFICULTY' : 'sum', 'CHEST PAIN' : 'sum', 'LUNG_CANCER_YES' : 'sum'})
-df4 = df.groupby('YELLOW_FINGERS').agg({ 'GENDER_M' : 'sum','AGE' : 'count', 'SMOKING' : 'sum', 'YELLOW_FINGERS' : 'sum', 'ANXIETY' : 'sum',
-       'PEER_PRESSURE' : 'sum', 'CHRONIC DISEASE' : 'sum', 'FATIGUE' : 'sum', 'ALLERGY' : 'sum', 'WHEEZING' : 'sum',
-       'ALCOHOL CONSUMING' : 'sum', 'COUGHING' : 'sum', 'SHORTNESS OF BREATH' : 'sum',
-       'SWALLOWING DIFFICULTY' : 'sum', 'CHEST PAIN' : 'sum', 'LUNG_CANCER_YES' : 'sum'})
-df5 = df.groupby('ANXIETY').agg({ 'GENDER_M' : 'sum','AGE' : 'count', 'SMOKING' : 'sum', 'YELLOW_FINGERS' : 'sum', 'ANXIETY' : 'sum',
-       'PEER_PRESSURE' : 'sum', 'CHRONIC DISEASE' : 'sum', 'FATIGUE' : 'sum', 'ALLERGY' : 'sum', 'WHEEZING' : 'sum',
-       'ALCOHOL CONSUMING' : 'sum', 'COUGHING' : 'sum', 'SHORTNESS OF BREATH' : 'sum',
-       'SWALLOWING DIFFICULTY' : 'sum', 'CHEST PAIN' : 'sum', 'LUNG_CANCER_YES' : 'sum'})
-scaler = StandardScaler()
-scaler.fit(df.drop('LUNG_CANCER_YES',axis = 1))
-scaled_features = scaler.transform(df.drop('LUNG_CANCER_YES',axis = 1))
-df_feat = pd.DataFrame(scaled_features,columns = ['AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'PEER_PRESSURE',
-       'CHRONIC DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING',
-       'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH',
-       'SWALLOWING DIFFICULTY', 'CHEST PAIN',  'GENDER_M'])
-X = df_feat
-
-y = df['LUNG_CANCER_YES']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
-
-knn = KNeighborsClassifier(n_neighbors = 4)
-knn.fit(X_train,y_train)
-y_pred = knn.predict(X_test)
+model = ensemble.RandomForestClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 print('Accuracy : {}'.format(accuracy_score(y_test, y_pred)))
 
-joblib.dump(knn,r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\Cancer_API\cancer_model.pkl")
+clf_report = classification_report(y_test, y_pred)
+print('Classification report')
+print("---------------------")
+print(clf_report)
+print("_____________________")
 
-
-
-
-
-
-
-
-
-
-
-
-
-# 
+joblib.dump(model,r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\Cancer_API\cancer_model.pkl")
--- model_codes/liver.py
+++ model_codes/liver.py
@@ -1,54 +1,32 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn import ensemble
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn import linear_model
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.tree import DecisionTreeClassifier
 
+patients=pd.read_csv(r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\data\indian_liver_patient.csv")
+patients['Gender']=patients['Gender'].apply(lambda x:1 if x=='Male' else 0)
+patients=patients.fillna(0.94)
 
-df = pd.read_csv(r"C:\Users\Madhumitha\Desktop\MajorProject\dataset\Liver_data\indian_liver_patient.csv")
-df["Albumin_and_Globulin_Ratio"] = df.Albumin_and_Globulin_Ratio.fillna(df['Albumin_and_Globulin_Ratio'].mean())
+X=patients[['Total_Bilirubin', 'Direct_Bilirubin',
+       'Alkaline_Phosphotase', 'Alamine_Aminotransferase',
+       'Total_Protiens', 'Albumin', 'Albumin_and_Globulin_Ratio']]
+y=patients['Dataset']
 
-pd.get_dummies(df['Gender'], prefix = 'Gender').head()
-df = pd.concat([df,pd.get_dummies(df['Gender'], prefix = 'Gender')], axis=1)
-X = df.drop(['Gender','Dataset'], axis=1)
-y = df['Dataset']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=123)
 
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn import linear_model
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier
+print('Shape training set: X:{}, y:{}'.format(X_train.shape, y_train.shape))
+print('Shape test set: X:{}, y:{}'.format(X_test.shape, y_test.shape))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=101)
-print (X_train.shape)
+model = ensemble.RandomForestClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print('Accuracy : {}'.format(accuracy_score(y_test, y_pred)))
 
-# Random Forest
-
-random_forest = RandomForestClassifier(n_estimators=100)
-random_forest.fit(X_train, y_train)
-#Predict Output
-rf_predicted = random_forest.predict(X_test)
-
-random_forest_score = round(random_forest.score(X_train, y_train) * 100, 2)
-random_forest_score_test = round(random_forest.score(X_test, y_test) * 100, 2)
-print('Random Forest Test Score: \n', random_forest_score_test)
-print('Accuracy: \n', accuracy_score(y_test,rf_predicted))
-print(confusion_matrix(y_test,rf_predicted))
-print(classification_report(y_test,rf_predicted))
-
-joblib.dump(random_forest,r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\Liver_API\liver_model.pkl")
-
-
+clf_report = classification_report(y_test, y_pred)
+print('Classification report')
+print("---------------------")
+print(clf_report)
+print("_____________________")
+joblib.dump(model,r"C:\Users\Madhumitha\Desktop\Majorrrrrr\Health-App\Liver_API\liver_model.pkl")
